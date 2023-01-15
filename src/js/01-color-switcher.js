7b0766197db9c845a6bec2c3ab8903cf64d088c4
Vasiliy Pshenychniuk <vasiliy8@gmail.com>
2023-01-15 16:23:46 +0200

    01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const btnStartEl = document.querySelector('button[data-start]');
+const btnStopEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+btnStartEl.addEventListener('click', onBtnStartClick);
+btnStopEl.addEventListener('click', onBtnStopClick);
+
+let intervalId = null;
+
+function onBtnStartClick() {
+    intervalId = setInterval(() => {        
+        bodyEl.style.backgroundColor = `${getRandomHexColor()}`
+    }, 1000);
+    
+    btnStartEl.setAttribute("disabled", "");
+};
+
+function onBtnStopClick() { 
+    clearInterval(intervalId);
+    btnStartEl.removeAttribute("disabled");
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
